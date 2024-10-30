4aaf8746e6b6b118ebe01fe2643e15dd00ac9521
iraneldf <iranel831@gmail.com>
2024-10-30 16:28:51 -0400

    Ajustando Carrusel

--- src/components/About/CarruelItem.jsx
+++ src/components/About/CarruelItem.jsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function CarruelItem({icon, tittle, content, avatar, name, rol}) {
     return (
-        <div className='flex flex-col gap-[16px] p-[24px] lg:p-[32px] embla__slide'>
+        <div className='max-w-[100px] flex flex-col gap-[16px] p-[24px] lg:p-[32px] embla__slide'>
 
-            <div className='py-[16px] flex'>
+            <div className='py-[16px] flex items-center'>
                 <Image className='mr-[3px]' src={icon} alt={'icon'} width={30.4}
                        height={32}/>
                 <h1 className='font-bold text-[#475569] leading-[26.4px]'>{tittle}</h1>
             </div>
 
-            <p className='leading-[22.4px] lg:leading-[38.4px]'>{content}</p>
+            <p className='texto-limitado leading-[22.4px] lg:leading-[38.4px]'>{content}</p>
 
             <div className='flex pt-[16px] items-center gap-[16px]'>
                 <Image className='inline mr-[3px]' src={avatar} alt={'avatar'}
--- src/components/About/Carrusel2.jsx
+++ src/components/About/Carrusel2.jsx
@@ -14,13 +14,20 @@ const Carousel = () => {
             <div className="embla__container">
 
                 <CarruelItem
-                    icon={'/images/icons/carrusel/iconCarrusel1.svg'}
+                    icon={'/images/icons/carrusel/iconCarrusel5.svg'}
                     tittle={'Primero'}
-                    avatar={'/images/icons/carrusel/avatar1.svg'}
-                    name={'Hellena John'}
-                    rol={'Co-founder'}
-                    content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit\n' +
-                        '                purus venenatis velit semper lectus sed ornare quam nulla.'}/>
+                    avatar={'/images/icons/carrusel/avatar5.svg'}
+                    name={'Ralph Edwars'}
+                    rol={'Harvesting 32KWh '}
+                    content={'Vehicula sit sit pharetra bibendum ut risus accumsan. Purus, in metus, enim, ipsum morbi euismod pellentesque Mattis pharetra.'}/>
+
+                <CarruelItem
+                    icon={'/images/icons/carrusel/iconCarrusel3.svg'}
+                    tittle={'Zoomerr'}
+                    avatar={'/images/icons/carrusel/avatar3.svg'}
+                    name={'Hellen Jummy'}
+                    rol={'Team Lead'}
+                    content={'Non risus viverra enim, quis. Eget vitae arcu vivamus sit tellus, viverra turpis lorem. Varius a turpis urna id porttitor.'}/>
 
                 <CarruelItem
                     icon={'/images/icons/carrusel/iconCarrusel1.svg'}
@@ -28,35 +35,24 @@ const Carousel = () => {
                     avatar={'/images/icons/carrusel/avatar1.svg'}
                     name={'Hellena John'}
                     rol={'Co-founder'}
+                    content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit purus venenatis velit semper lectus sed ornare quam nulla.'}/>
+
+                <CarruelItem
+                    icon={'/images/icons/carrusel/iconCarrusel2.svg'}
+                    tittle={'ArtVenue'}
+                    avatar={'/images/icons/carrusel/avatar2.svg'}
+                    name={'David Oshodi'}
+                    rol={'Manager'}
                     content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit\n' +
                         '                purus venenatis velit semper lectus sed ornare quam nulla.'}/>
 
                 <CarruelItem
-                    icon={'/images/icons/carrusel/iconCarrusel1.svg'}
-                    tittle={'SHELLS'}
-                    avatar={'/images/icons/carrusel/avatar1.svg'}
-                    name={'Hellena John'}
-                    rol={'Co-founder'}
-                    content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit\n' +
-                        '                purus venenatis velit semper lectus sed ornare quam nulla.'}/>
-
-                <CarruelItem
-                    icon={'/images/icons/carrusel/iconCarrusel1.svg'}
-                    tittle={'SHELLS'}
-                    avatar={'/images/icons/carrusel/avatar1.svg'}
-                    name={'Hellena John'}
-                    rol={'Co-founder'}
-                    content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit\n' +
-                        '                purus venenatis velit semper lectus sed ornare quam nulla.'}/>
-
-                <CarruelItem
-                    icon={'/images/icons/carrusel/iconCarrusel1.svg'}
-                    tittle={'Ultimo'}
-                    avatar={'/images/icons/carrusel/avatar1.svg'}
-                    name={'Hellena John'}
-                    rol={'Co-founder'}
-                    content={'Aliquet ridiculus mi porta habitant vulputate rhoncus, mattis amet enim. Sit\n' +
-                        '                purus venenatis velit semper lectus sed ornare quam nulla.'}/>
+                    icon={'/images/icons/carrusel/iconCarrusel4.svg'}
+                    tittle={'WAVES'}
+                    avatar={'/images/icons/carrusel/avatar4.svg'}
+                    name={'Jane Doe'}
+                    rol={'Senior Designer'}
+                    content={'Magna egestas aliquet ut integer non. Sed diam enim nibh sit. Aliquam laoreet aenean metus nibh eu scelerisque.'}/>
 
 
             </div>
